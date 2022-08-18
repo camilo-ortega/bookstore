3576c67ad249c506602434c22e12964aa9de70ed
Camilo Ortega <camiloortega@Camilos-MacBook-Air.local>
2022-08-18 11:03:23 -0500

    add pagination in card swiper

--- lib/ui/screens/widgets/card_swiper.dart
+++ lib/ui/screens/widgets/card_swiper.dart
@@ -1,10 +1,10 @@
 import 'package:bookstore/domain/models/book/book.dart';
 import 'package:bookstore/domain/models/book/book_new_response.dart';
-import 'package:bookstore/ui/constants/images.dart';
 import 'package:bookstore/ui/helpers/screen.dart';
 import 'package:bookstore/ui/providers/book_provider.dart';
 import 'package:bookstore/ui/routers/app_navigator.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_card_swipper/flutter_card_swiper.dart';
 import 'package:provider/provider.dart';
 
@@ -34,32 +34,80 @@ class CardSwiper extends StatelessWidget {
 
         return SizedBox(
           width: double.infinity,
-          height: Screen.getHeightInPercent(60),
-          child: Swiper(
-            itemCount: books.length,
-            layout: SwiperLayout.STACK,
-            itemWidth: Screen.getWidthInPercent(80),
-            itemHeight: Screen.getHeightInPercent(50),
-            itemBuilder: (BuildContext context, int index) {
-              final book = books[index];
-
-              return GestureDetector(
-                onTap: () {
-                  appNavigator.toBookDetailScreen(book);
-                },
-                child: Hero(
-                  tag: book.isbn13,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(20),
-                    child: FadeInImage(
-                      placeholder: const AssetImage(Images.noImage),
-                      image: NetworkImage(book.image),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
+          height: Screen.getHeightSize,
+          child: Column(
+            children: [
+              SizedBox(
+                height: Screen.getHeightInPercent(5),
+              ),
+              Text(
+                'New book releases',
+                style: TextStyle(
+                    fontSize: Screen.getDiagonalInPercent(2),
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: Screen.getHeightInPercent(2),
+              ),
+              Swiper(
+                pagination: const SwiperPagination(
+                  alignment: Alignment.bottomCenter,
+                  builder: DotSwiperPaginationBuilder(
+                      color: Colors.grey, activeColor: Colors.indigo),
                 ),
-              );
-            },
+                layout: SwiperLayout.CUSTOM,
+                customLayoutOption:
+                    CustomLayoutOption(startIndex: -1, stateCount: 3).addRotate(
+                        [-45.0 / 180, 0.0, 45.0 / 180]).addTranslate(const [
+                  Offset(-370.0, -40.0),
+                  Offset(0.0, 0.0),
+                  Offset(370.0, -40.0)
+                ]),
+                itemCount: books.length > 9 ? 10 : books.length,
+                itemWidth: Screen.getWidthSize,
+                itemHeight: Screen.getHeightInPercent(50),
+                itemBuilder: (BuildContext context, int index) {
+                  final book = books[index];
+
+                  return Container(
+                    margin: EdgeInsets.all(
+                      Screen.getWidthInPercent(5),
+                    ),
+                    child: Column(
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            appNavigator.toBookDetailScreen(book);
+                          },
+                          child: Hero(
+                            tag: book.isbn13,
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(20),
+                              child: Column(
+                                children: [
+                                  FadeInImage(
+                                    placeholder: NetworkImage(book.image),
+                                    image: NetworkImage(book.image),
+                                    fit: BoxFit.contain,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                        Text(
+                          '${index + 1}',
+                          style: TextStyle(
+                            fontSize: Screen.getDiagonalInPercent(1.5),
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ],
           ),
         );
       },
